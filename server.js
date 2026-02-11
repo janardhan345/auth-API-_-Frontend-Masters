316ddef410aba6d5ec36426bc90b1e4bb7a8f3df
Janardhan B <147693850+janardhan345@users.noreply.github.com>
2026-02-11 19:05:20 +0530

    Login Function created and tested successfully.

--- server.js
+++ server.js
@@ -28,8 +28,46 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-// ADD HERE THE REST OF THE ENDPOINTS
+function findUser(email) {
+  const results = db.data.users.filter(u=>u.email==email);
+  if(results.lenght==0) return undefined;
+  return results[0];
+}
+app.post("/auth/login", (req, res)=> {
+  const userFound = findUser(req.body.email);
+if(userFound){
+  if(bcrypt.compareSync(req.body.password, userFound.password)){
+    res.send({ok:true, name:userFound.name, email:userFound.email});
+  }
+  else{
+      res.send({ok:false, message:"Credentials are wrong"});
+  }
+}
+else{
+  res.send({ok:false, message: "credentials are wrong"});
+}
+});
 
+// ADD HERE THE REST OF THE ENDPOINTS
+app.post("/auth/register", (req, res)=> {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPass = bcrypt.hashSync(req.body.password, salt);
+
+  //TODO: Data validation
+  const user = {
+    name:req.body.name,
+    email:req.body.email,
+    password:hashedPass
+  }
+  const userFound = findUser(user.email);
+  if(userFound){
+    res.send({ok: false, message:"User already exists"});
+  }
+  else{ 
+  db.data.users.push(user);
+  db.write();
+  }
+});
 
 
 app.get("*", (req, res) => {
